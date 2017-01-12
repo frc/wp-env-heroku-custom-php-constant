aa343186763049d047ad4f1952028afcce963616
Josep Roca <josep.roca@frantic.com>
2017-01-12 15:51:11 +0200

    minor variable renames

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Allow PHP constants to be defined through Heroku environment, as long as the environment key starts with FRC_PHP_CONST_ prefix.
 
-The prefix gets removed when the constant is set in the PHP side. For example, an environment variable FRC_PHP_CONST_PLL_LANGUAGE will define the PHP constant PLL_LANGUAGE within WordPress (with the value coming from the env key).
+The prefix gets removed when the constant is set in the PHP side. For example, an environment variable FRC_PHP_CONST_PLL_COOKIE will define the PHP constant PLL_COOKIE within WordPress by using the getenv function (with the value coming from the env key).
--- src/PHPConst.php
+++ src/PHPConst.php
@@ -2,11 +2,11 @@
 
 namespace Frc\WP\Env\Heroku\PHPConst;
 
-$all_frc_php_const_env = array_filter(array_keys($_ENV), function ($k) {
+$all_frc_php_const_env_keys = array_filter(array_keys($_ENV), function ($k) {
     return preg_match('/^FRC_PHP_CONST_/', $k);
 });
 
-foreach ($all_frc_php_const_env as $php_const_env) {
-    $constant_name = preg_replace('/^FRC_PHP_CONST_/', '', $php_const_env);
-    define($constant_name, getenv($php_const_env));
+foreach ($all_frc_php_const_env_keys as $php_const_env_key) {
+    $constant_name = preg_replace('/^FRC_PHP_CONST_/', '', $php_const_env_key);
+    define($constant_name, getenv($php_const_env_key));
 }
