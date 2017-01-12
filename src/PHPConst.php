05da956777818fdab9088ad22a8fb47c416d4c61
Josep Roca <josep.roca@frantic.com>
2017-01-12 16:48:04 +0200

    treat true/false strings as boolean

--- src/PHPConst.php
+++ src/PHPConst.php
@@ -8,5 +8,14 @@ $all_frc_php_const_env_keys = array_filter(array_keys($_ENV), function ($k) {
 
 foreach ($all_frc_php_const_env_keys as $php_const_env_key) {
     $constant_name = preg_replace('/^FRC_PHP_CONST_/', '', $php_const_env_key);
-    define($constant_name, getenv($php_const_env_key));
+    $constant_value = getenv($php_const_env_key);
+
+    // Treat true/false strings as boolean values
+    if ($constant_value && strtolower($constant_value) === 'false') {
+        $constant_value = false;
+    } elseif ($constant_value && strtolower($constant_value) === 'true') {
+        $constant_value = true;
+    }
+
+    define($constant_name, $constant_value);
 }
